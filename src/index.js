ec514064a75e36f0e4abf5343c1d1860a6abc2e5
korbann <korbutann@gmail.com>
2018-09-22 21:16:49 +0300

    another one

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 module.exports = function warmup(temperature) {
-    if (temperature == -20) return -4;
-    if (temperature == 0) return 32;
-    if (temperature == 15) return 59;
-    if (temperature == 40) return 104;
+   temperature = temperature *(9/5)+32;
+  Math.floor (temperature);
+  return temperature;
 };
